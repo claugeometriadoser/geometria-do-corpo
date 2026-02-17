83bf1df8c5c219d3ea61ece57abf02ee8b0b4423
claugeometriadoser <barrosclau@gmail.com>
2026-02-17 15:28:16 -0300

    Create semana-atual.tsx

---
+++ src/pages/semana-atual.tsx
@@ -0,0 +1,8 @@
+export default function SemanaAtual() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Semana Atual</h1>
+      <p>Aqui aparecerá o plano de 4 semanas.</p>
+    </div>
+  );
+}
