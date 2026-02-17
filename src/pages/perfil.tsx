d4d10d0ad069c11635a34839c3d849b9ed1d6a4b
claugeometriadoser <barrosclau@gmail.com>
2026-02-17 15:28:44 -0300

    Create perfil.tsx

---
+++ src/pages/perfil.tsx
@@ -0,0 +1,8 @@
+export default function Perfil() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Perfil</h1>
+      <p>Aqui você configurará tempo, energia e restrições.</p>
+    </div>
+  );
+}
