9935442c063dd94e6d9ea547068d7f8c8da893e6
claugeometriadoser <barrosclau@gmail.com>
2026-02-17 16:00:10 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div style={{ padding: 24 }}>
       <h1>Geometria do Corpo</h1>
       <p>Seu app está funcionando.</p>
+
       <p>Escolha uma tela:</p>
       <a href="/treino-do-dia">Treino do Dia</a><br />
       <a href="/semana-atual">Semana Atual</a><br />
