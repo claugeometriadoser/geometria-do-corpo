0e12d29de11e73d2e07268bc8c449023f1ea89e4
claugeometriadoser <barrosclau@gmail.com>
2026-02-17 17:02:04 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-semibold text-[#5A4637]">
+        Geometria do Corpo
+      </h1>
+
+      <p className="mt-4">Home carregando com Tailwind funcionando ✨</p>
+    </div>
+  )
+}
