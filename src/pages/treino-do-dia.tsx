4d9249f45cb56eb267ed037f0789ec7b173adb54
claugeometriadoser <barrosclau@gmail.com>
2026-02-17 15:27:53 -0300

    Create treino-do-dia.tsx

---
+++ src/pages/treino-do-dia.tsx
@@ -0,0 +1,8 @@
+export default function TreinoDoDia() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Treino do Dia</h1>
+      <p>Exercícios baseados no protocolo.</p>
+    </div>
+  );
+}
